2e280f9f72e90903bad3a2b5e1a9091080866f82
Lei Wen <lei.wen@outlook.com>
2024-02-24 22:46:51 -0500

    Add project files.

---
+++ NCLCapture/NclApiClient.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NCLCapture
+{
+    public class NclApiClient
+    {
+        
+         public async Task<string> GetVcationItineraries()
+        {
+
+            string request = "https://www.ncl.com/api/vacations/v2/itineraries?guests=2";
+
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+            }
+
+            return "yes";
+
+        }
+
+        public async Task<string>  Getitinerary(string code)
+        {
+            string request = " https://www.ncl.com/api/vacations/v2/search-result-itinerary/GETAWAY5NYCWRFNYC?guests=2&v=332056977-1694115153843";
+
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(request);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+            }
+
+            return "yes";
+        }
+
+
+    }
+}
---
+++ NCLCapture/Program.cs
@@ -0,0 +1,18 @@
+﻿using
+
+    // See https://aka.ms/new-console-template for more information
+
+
+   string request =  "https://www.ncl.com/api/vacations/v2/itineraries?guests=2";
+    https://www.ncl.com/api/vacations/v2/search-result-itinerary/GETAWAY5NYCWRFNYC?guests=2&v=332056977-1694115153843
+
+HttpClient client = new HttpClient();
+    string  request = "https://www.ncl.com/vacations?cruise-port=nyc&duration=5-8&sort=price_low_high";
+var response = await client.GetAsync(request);
+
+    if(response.IsSuccessStatusCode)
+{
+    var result = await response.Content.ReadAsStringAsync();
+}
+
+Console.WriteLine("Hello, World!");
